f9e1267df40d151b5d2d27323ba4eac8945f5fa9
joshyeom <jhyeom95@daum.net>
2024-02-05 15:51:55 +0900

    feat: text highlight

--- src/app/_components/Search.tsx
+++ src/app/_components/Search.tsx
@@ -30,24 +30,38 @@ const Search = ({items}: any) => {
       }, [keyword, items]);
 
     return (
-        <main className="w-3/12 flex flex-col items-center">
+        <main className="w-760px flex flex-col items-center">
             <Image src="https://osmlib.com/d5386814b15c0b011cd619fd05125d45.png" alt="logo" width={400} height={100}/>
             <input 
                 type="text" 
                 onChange={changeHandler}
                 value={keyword}
-                placeholder="장갑 공격력 주문서 60%"
+                placeholder="검색어를 입력해 주세요"
                 className="w-full h-9 border-white border bg-transparent text-white placeholder:italic placeholder:text-slate-400 pl-5 py-6 mt-10 rounded-t-lg"
             />
-            {keyword.length > 0 ? 
+            {list.length > 0 ? 
                 <ul className="w-full border-white border border-t-0 flex flex-col justify-center align-center">
                     <span className="text-center mt-5 text-white">정보</span>
                     {
-                        list.map((v: any) => 
-                        (<li key={v[0]} className="flex item-center h-20 p-4 pl-7 hover:bg-red-600">    
-                                <Image src={v[1]} alt={v[0]} width={40} height={30}/>
-                                <div className="pl-7 text-lg text-white leading-10">{v[0]}</div>
-                            </li>)
+                        list.map((v: string[]) => {
+                            const index = v[0].indexOf(keyword)
+                            const front = v[0].slice(0, index)
+                            const highlight = v[0].slice(index, index + keyword.length)
+                            const end = v[0].slice(index + keyword.length, v[0].length)
+                            console.log(index, highlight ,front, end)
+                            return (
+                            <li key={v[0]} className="relative group flex item-center h-20 p-4 pl-7 hover:bg-red-600">    
+                            <Image src={v[1]} alt={v[0]} width={40} height={30}/>
+                            <div className="pl-7 text-lg text-white leading-10">
+                                <span>{front}</span>
+                                <span className="text-red-600 group-hover:hidden">{highlight}</span>
+                                <span className="text-white hidden group-hover:inline">{highlight}</span>
+                                <span>{end}</span>
+                            </div>
+                            </li>
+                        )
+                        }
+                        
                         )
                     }
                 </ul>
