86ad3450f2244a6aacd491933397d3cf10415ab4
joshyeom <jhyeom95@daum.net>
2024-03-05 17:53:55 +0900

    decoded %

--- src/app/_utils/fetchImageURL.ts
+++ src/app/_utils/fetchImageURL.ts
@@ -1,9 +1,10 @@
 import { DataProps } from "../types/DataProps"
 
 export const fetchImageURL = (name: string, items: DataProps[]) => {
-    const decoded = decodeURI(name)
-    console.log(decoded)
-    const filteredData = items.filter((v) => v.name === decoded)[0]
+    if(name.startsWith("%")){
+        name = decodeURI(name)
+    }
+    const filteredData = items.filter((v) => v.name === name)[0]
         if(filteredData){
             const imageURL = filteredData.imageUrl
             return imageURL
